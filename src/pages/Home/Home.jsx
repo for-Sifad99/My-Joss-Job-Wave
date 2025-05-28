580c2be27b02d33928baf8cfab41781ce2a6d33c
Si Fad <sifayed00@gmail.com>
2025-05-29 02:53:05 +0600

    register route update.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,11 +6,10 @@ const Home = () => {
         <>
         {/* Helmet */}
             <Helmet>
-                <title>Your Dream Job Awaits - Job Wave</title>
-                <meta name="description" content="Job Wave is your one-stop job portal connecting job seekers and employers. Find jobs, apply online, and build your future today!" />
-                <meta name="keywords" content="jobs, hiring, job portal, career, apply, employers, job seekers" />
-                <meta name="author" content="Job Wave Team" />
+                <title>Find Your Dream Job - Job Wave</title>
+                <meta name="description" content="Job Wave is a modern job portal for job seekers and employers. Discover opportunities, apply instantly, and surf your career forward!" />
             </Helmet>
+
 
             {/* Content */}
             <section className='h-[500px] my-20'>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { FiUser, FiMail, FiLock, FiEyeOff, FiEye } from "react-icons/fi";
 import { Link } from "react-router";
+import registerLottie from '../../assets/lotties/register.json';
+import { Helmet } from "react-helmet-async";
+import Lottie from "lottie-react";
 
 const Register = () => {
     const [showPassword, setShowPassword] = useState(false);
@@ -10,69 +13,82 @@ const Register = () => {
     };
 
     return (
-        <div className="flex items-center justify-center py-10">
-            <div className="bg-white dark:bg-[var(--color-text-copy)] shadow-xl rounded-[100px] sm:py-16 py-10 sm:px-10 px-6 w-full max-w-md mx-4">
-                <h2 className="sm:text-3xl text-[28px] font-bold text-center text-[var(--color-light-accent)] dark:text-slate-300 mb-6">Create Account</h2>
-                <form onSubmit={handleSubmit} className="space-y-5">
-                    <div>
-                        <label className="text-base block mb-1 font-bold text-[var(--color-light-accent)] dark:text-[var(--color-dark-accent)]">Full Name</label>
-                        <div className="relative">
-                            <FiUser className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
-                            <input
-                                type="text"
-                                name="name"
-                                required
-                                placeholder="Your Name"
-                                className="text-sm w-full pl-10 pr-3 py-2 rounded-2xl border-2 border-[var(--color-light-accent)] dark:[var(--color-dark-accent)] focus:outline-none focus:border-3 dark:bg-[#23272f] dark:text-slate-300"
-                            />
+        <>
+        {/* Helmet */}
+            <Helmet>
+                <title>Join Job Wave - Register</title>
+                <meta name="description" content="Create your Job Wave account and unlock endless job opportunities tailored just for you." />
+            </Helmet>
+
+
+            <div className="flex md:flex-row flex-col-reverse items-center justify-center py-10 md:px-24">
+                {/* Form Content */}
+                <div className="bg-white dark:bg-[var(--color-text-copy)] border-2 border-[#ced8ff] dark:border-none shadow-xl rounded-[100px] sm:py-16 py-10 sm:px-10 px-6 w-full max-w-md mx-4">
+                    <h2 className="sm:text-3xl text-[28px] font-bold text-center text-[var(--color-light-accent)] dark:text-slate-300 mb-6">Create Account!</h2>
+                    <form onSubmit={handleSubmit} className="space-y-5">
+                        <div>
+                            <label className="text-base block mb-1 font-bold text-[var(--color-light-accent)] dark:text-[var(--color-dark-accent)]">Full Name</label>
+                            <div className="relative">
+                                <FiUser className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
+                                <input
+                                    type="text"
+                                    name="name"
+                                    required
+                                    placeholder="Your Name"
+                                    className="text-sm w-full pl-10 pr-3 py-2 rounded-2xl border-2 border-[var(--color-light-accent)] dark:[var(--color-dark-accent)] focus:outline-none focus:border-3 dark:bg-[#23272f] dark:text-slate-300"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <label className="text-base block mb-1 font-bold text-[var(--color-light-accent)] dark:text-[var(--color-dark-accent)]">Email</label>
-                        <div className="relative">
-                            <FiMail className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
-                            <input
-                                type="email"
-                                name="email"
-                                required
-                                placeholder="you@email.com"
-                                className="text-base w-full pl-10 pr-3 py-2 rounded-2xl border-2 border-[var(--color-light-accent)] dark:[var(--color-dark-accent)] focus:outline-none focus:border-3 dark:bg-[#23272f] dark:text-slate-300"
-                            />
+                        <div>
+                            <label className="text-base block mb-1 font-bold text-[var(--color-light-accent)] dark:text-[var(--color-dark-accent)]">Email</label>
+                            <div className="relative">
+                                <FiMail className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
+                                <input
+                                    type="email"
+                                    name="email"
+                                    required
+                                    placeholder="you@email.com"
+                                    className="text-base w-full pl-10 pr-3 py-2 rounded-2xl border-2 border-[var(--color-light-accent)] dark:[var(--color-dark-accent)] focus:outline-none focus:border-3 dark:bg-[#23272f] dark:text-slate-300"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <label className="text-base block mb-1 font-bold text-[var(--color-light-accent)] dark:text-[var(--color-dark-accent)]">Password</label>
-                        <div className="relative">
-                            <FiLock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
-                            <input
-                                type={showPassword ? "text" : "password"}
-                                name="password"
-                                required
-                                placeholder="Password"
-                                className="text-base w-full px-10 py-2 rounded-2xl border-2 border-[var(--color-light-accent)] dark:[var(--color-dark-accent)] focus:outline-none focus:border-3 dark:bg-[#23272f] dark:text-slate-300"
-                            />
-                            <button
-                                type="button"
-                                className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400"
-                                onClick={() => setShowPassword(!showPassword)}
-                                tabIndex={-1}
-                            >
-                                {showPassword ? <FiEyeOff /> : <FiEye />}
-                            </button>
+                        <div>
+                            <label className="text-base block mb-1 font-bold text-[var(--color-light-accent)] dark:text-[var(--color-dark-accent)]">Password</label>
+                            <div className="relative">
+                                <FiLock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    name="password"
+                                    required
+                                    placeholder="Password"
+                                    className="text-base w-full px-10 py-2 rounded-2xl border-2 border-[var(--color-light-accent)] dark:[var(--color-dark-accent)] focus:outline-none focus:border-3 dark:bg-[#23272f] dark:text-slate-300"
+                                />
+                                <button
+                                    type="button"
+                                    className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    tabIndex={-1}
+                                >
+                                    {showPassword ? <FiEyeOff /> : <FiEye />}
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <button
-                        type="submit"
-                        className="text-sm w-full py-2 bg-[var(--color-light-accent)] hover:bg-linear-to-r/srgb hover:from-indigo-500 hover:to-indigo-400 hover:bg-blue-700 text-white font-semibold rounded-2xl transition-all duration-200"
-                    >
-                        Register
-                    </button>
-                </form>
-                <p className="mt-4 text-center text-gray-600 dark:text-gray-300 sm:text-sm text-xs">
-                    Already have an account? <Link to="/login" className="text-blue-800 dark:text-blue-400 underline animate-pulse">Log in</Link>
-                </p>
+                        <button
+                            type="submit"
+                            className="text-sm w-full py-2 bg-[var(--color-light-accent)] hover:bg-linear-to-r/srgb hover:from-indigo-500 hover:to-indigo-400 hover:bg-blue-700 text-white font-semibold rounded-2xl transition-all duration-200"
+                        >
+                            Register
+                        </button>
+                    </form>
+                    <p className="mt-4 text-center text-gray-600 dark:text-gray-300 sm:text-sm text-xs">
+                        Already have an account? <Link to="/login" className="text-blue-800 dark:text-blue-400 underline animate-pulse">Log in</Link>
+                    </p>
+                </div>
+
+                {/* Lottie Animation */}
+                <Lottie animationData={registerLottie} className="w-full lg:max-w-md md:w-[440px] sm:max-w-md lg:mb-0 mb-8"></Lottie>
             </div>
-        </div>
+        </>
     );
 };
 
